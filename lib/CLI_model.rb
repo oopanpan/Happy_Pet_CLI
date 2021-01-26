78f5502c36526d052355c3abe978de260f3d292b
sin wan wildy <sw.wildy@gmail.com>
2021-01-26 14:33:12 -0800

    sintue2

--- lib/CLI_model.rb
+++ lib/CLI_model.rb
@@ -13,11 +13,9 @@ class CommandLineInterface
     end
 
     def menu_option(user)
-        puts "Press 1 to "
-        puts "Press 2 to "
-        puts "Press 3 to "
-        puts "Press 4 to "
-        puts "Press 5 to "
+        puts "Press 1 to ADD PETS!"
+        puts "Press 2 to VIEW PETS!"
+        puts "Press 0 to exit! "
         input = STDIN.gets.chomp()
         puts self.user_input(user, input)
     end
@@ -25,15 +23,9 @@ class CommandLineInterface
     def user_input(user, input)
         case input
         when "1"
-            METHOD NAME 
+            add_pet(user)  
         when "2"
-            METHOD NAME 
-        when "3"
-            METHOD NAME 
-        when "4" 
-            METHOD NAME 
-        when "5"
-            METHOD NAME 
+            view_pet(user)
         when "0"
             exit 
         else 
@@ -46,6 +38,62 @@ class CommandLineInterface
         "\n\nGOODBYE! HAVE A PRODUCTIVE DAY!"
     end
     
+    #this method helps user to create a pet, returning pet instance 
 
+    def add_pet(user)
+        puts "\n\nTo add a pet, Please enter the pet name: "
+        input = STDIN.gets.chomp()
+        new_pet = Pet.new(name:input)
+        new_pet.user_id = self.id
+        put "Please enter #{input} species: "
+        new_pet.species = STDIN.gets.chomp()
+        puts "Please enter #{input} age: "
+        new_pet.age = STDIN.gets.chomp()
+        new_pet.save
+        puts "Pet #{input} created!"
+        add_routine(user)
+    end
+
+    def add_routine(user)
+        puts "\n\nPlease add a routine: "
+        input = STDIN.gets.chomp()
+        new_routine = Routine.new(name:input)
+        puts "Please enter #{input} description: "
+        new_routine.description = STDIN.gets.chomp()
+        puts "Pet #{input} routine created!"
+        routine_menu_option(user)
+    end
+
+    def view_current_routine(pet,user)
+        Routine.find_by(user_id:user.id, pet_id:pet.id)
+    end
+
+
+    def routine_menu_option(user)
+        puts "Press 1 to ADD ROUTINE!"
+        puts "Press 2 to VIEW ALL ROUTINE!"
+        puts "Press 0 to exit! "
+        input = STDIN.gets.chomp()
+        puts self.routine_user_input(user, input)
+    end
+
+    def routine_user_input(user, input)
+        case input
+        when "1"
+            add_routine(user)  
+        when "2"
+            view_current_routine(user)
+        when "0"
+            exit 
+        else 
+            print "#{input} INVALID OPTION. PLEASE REVIEW THE MENU OPTIONS. \n\n"
+            routine_menu_option(user)
+        end
+    end
+ 
+    
+    def view_pet(user)
+
+    end
 
 end
--- lib/pet.rb
+++ lib/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ActiveRecord::Base
     has_many :routines
-    #has_many :users, through: :routines 
+    has_many :users, through: :routines 
 end
