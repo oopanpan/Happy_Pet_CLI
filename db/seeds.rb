0b039a9b82fef2b85b13a9438302244f2a287003
oopanpan <oopanpan@gmail.com>
2021-01-28 17:37:21 -0800

    seed_file_adjusted

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.destroy_all
 Pet.destroy_all
 Routine.destroy_all
 
-10.times do
-    User.create(name:Faker::Name.name)
-end
+User.create(name: "Pan")
+User.create(name: "Sin")
+
 
 5.times do
     Pet.create(name:Faker::Name.first_name, age:Faker::Number.within(range: 1..10), species: "dog")
@@ -14,10 +14,10 @@ end
     Pet.create(name:Faker::Name.first_name, age:Faker::Number.within(range: 1..10), species: "cat")
 end
 
-5.times do
-    Routine.create(name:Faker::Verb.simple_present, description:Faker::Food.description, if_complete:true, user_id:1, pet_id:Faker::Number.within(range: 1..2), on_date:nil)
+30.times do
+    Routine.create(name:Faker::Verb.simple_present, description:Faker::Food.description, if_complete:true, user_id:1, pet_id:Faker::Number.within(range: 1..10), on_date:nil)
 end
 
-5.times do
-    Routine.create(name:Faker::Verb.simple_present, description:Faker::Food.description, if_complete:false, user_id:1, pet_id:Faker::Number.within(range: 1..2), on_date:nil)
+30.times do
+    Routine.create(name:Faker::Verb.simple_present, description:Faker::Food.description, if_complete:false, user_id:2, pet_id:Faker::Number.within(range: 1..10), on_date:nil)
 end
